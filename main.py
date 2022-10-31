164f3c3c845fae6e5a18ee207936598d46e2fbc7
dodoeon <me@doeon.net>
2022-10-31 15:35:53 +0900

    720p

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from http.client import CannotSendHeader
+
 import cv2
-from cv2 import bilateralFilter
-import numpy as np
 
 Drive = cv2.VideoCapture('drive.mp4')
-roi = cv2.imread('roi.png')
+roi = cv2.imread('roi_720.png')
 
 while Drive.isOpened():
     run, frame = Drive.read()
@@ -13,17 +11,19 @@ while Drive.isOpened():
         break
     RGB = cv2.cvtColor(frame, cv2.IMREAD_COLOR)
     HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower = (13, 10, 195)
-    upper = (27, 45, 255)
+    lower = (0, 0, 190)
+    upper = (120, 30, 255)
 
-    mask = cv2.bitwise_and(HSV, roi)
+    mask = cv2.bilateralFilter(HSV, 2, 10, 10)
+    mask = cv2.GaussianBlur(mask, (3, 3), 0)
+
+    mask = cv2.bitwise_and(mask, roi)
     mask = cv2.inRange(mask, lower, upper)
 
-    mask = bilateralFilter(mask, -1, 10, 5)
-    mask = cv2.Canny(mask, 100, 200)
+    mask = cv2.Canny(mask, 200, 200)
 
-    RGB = cv2.resize(RGB, (640, 360))
-    mask = cv2.resize(mask, (640, 360))
+    RGB = cv2.resize(RGB, (960, 540))
+    mask = cv2.resize(mask, (960, 540))
     cv2.imshow('mask', mask)
     cv2.imshow('video', RGB)
     if cv2.waitKey(30) & 0xFF == ord('q'):
